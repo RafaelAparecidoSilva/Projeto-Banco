de269a39e3c00480d63917426d4e0109885424fe
Rafael Silva <raparecido@img.com.br>
2024-11-20 19:58:01 -0300

    Implementação do método sacar da classe Conta

--- models/conta.py
+++ models/conta.py
@@ -44,15 +44,36 @@ class Conta:
     def saldo_total(self) -> float:
         return self.__saldo_total
 
+    @saldo_total.setter
+    def saldo_total(self, valor: float) -> None:
+        self.__saldo_total = valor
+
     @property
     def _calcula_saldo_total(self) -> float:
         return self.saldo + self.limite
 
     def depositar(self, valor: float) -> None:
-        pass
+        if valor > 0:
+            self.saldo = self.saldo + valor
+            self.saldo_total = self._calcula_saldo_total
+            print('Depósito efetuado com sucesso!')
+        else:
+            print('Erro ao efetuar depósito. Tente novamente.')
 
     def sacar(self, valor: float) -> None:
-        pass
+        if valor > 0 and self.saldo_total >= valor:
+            if self.saldo >= valor:
+                self.saldo = self.saldo - valor
+                self.saldo_total = self._calcula_saldo_total
+            else:
+                restante: float = self.saldo - valor
+                self.limite = self.limite + restante
+                self.saldo = 0
+                self.saldo_total = self._calcula_saldo_total
+                print('Saque efetuado com sucesso.')
+
+        else:
+            print('Saque não realizado. Tente novamente.')
 
     def transferir(self, destino: object, valor: float) -> None:
         pass
